d31a0c638a653263a8feec9697459b0d71863ee8
ceejaymoss <130158448+ceejaymoss@users.noreply.github.com>
2025-02-18 06:58:02 +0000

    installed packages

--- Notebooks in Azure-ml/Tute_Load_Script_1.py
+++ Notebooks in Azure-ml/Tute_Load_Script_1.py
@@ -35,4 +35,26 @@ metrics = run.get_metrics()
 print(json.dumps(metrics, indent=2))
 #
 
+#
+# Import the needed packages from the Azure ML SDK
+from azureml.core import Experiment, ScriptRunConfig, Environment
+from azureml.core.conda_dependencies import CondaDependencies
+
+# Create a Python environment for the experiment
+env = Environment(" Insert Environment Here ")
+
+# Make sure the needed packages are installed 
+packages = CondaDependencies.create(conda_packages['scikit-learn', 'pip'], pip_packages=['azureml-defaults'])
+env.python.conda_dependencies = packages
+
+# Load the script we want to run
+script_config = ScriptRunConfig(source_directory='.',
+                                script= 'Insert Training Script here',
+                                environment=env)
+
+# Run the experiment
+experiment = Experiment(workspace=ws, name='Insert Experiment name here')
+run = experiment.submit(config=script_config)
+run.wait_for_completion()
+#
 
