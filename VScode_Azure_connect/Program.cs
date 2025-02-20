e9edc5a1ec462029ed0695213f4b5e142476f7d5
ceejaymoss <130158448+ceejaymoss@users.noreply.github.com>
2025-02-20 08:46:19 +0000

    azure vscode startup

---
+++ VScode_Azure_connect/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using System.IO;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionstring = "REPLACE ME";
+
+            var container = new BlobContainerClient(connectionstring, "demo");
+            await container.CreateIfNotExistsAsync();
+
+            var filename = "demo.txt";
+            var blob = container.GetBlobClient(filename);
+            await blob.UploadAsync(Path.Combine("data", filename), true);
+
+            await blob.DownloadToAsync(Path.Combine("Downloads", filename)); 
+
+        }
+    }
+}
