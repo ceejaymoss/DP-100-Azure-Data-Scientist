9f55b8799e6c666769c870bcc76ad066149e2a3b
ceejaymoss <130158448+ceejaymoss@users.noreply.github.com>
2025-02-26 07:20:54 +0000

    compute clusters

---
+++ Azure_databricks_filestore/Working-with-compute-clusters.py
@@ -0,0 +1,67 @@
+# # in bash terminal in an IDE
+# git clone https://github.com/aislanmano/mslearn-dp100
+
+#
+# load workspace
+import azureml.core
+from azureml.core import Workspace
+
+# Load the workspace from the saved config file
+ws = Workspace.from_config()
+print('Ready to use Azure ML {} to work with {}'.format(azureml.core.VERSION, ws.name))
+#
+
+#
+# Source compute
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core.compute_target import ComputeTargetException
+
+cluster_name = "your-compute-cluster"
+
+try:
+    # Check for existing compute target
+    training_cluster = ComputeTarget(workspace=ws, name=cluster_name)
+    print('Found existing cluster, use it.')
+except ComputeTargetException:
+    # If it doesn't already exist, create it
+    try:
+        compute_config = AmlCompute.provisioning_configuration(vm_size='STANDARD_DS11_V2', max_nodes=2)
+        training_cluster = ComputeTarget.create(ws, cluster_name, compute_config)
+        training_cluster.wait_for_completion(show_output=True)
+    except Exception as ex:
+        print(ex)
+#
+
+#
+# Run dataset
+from azureml.core import Dataset
+
+default_ds = ws.get_default_datastore()
+
+if 'diabetes dataset' not in ws.datasets:
+    default_ds.upload_files(files=['./data/diabetes.csv', './data/diabetes2.csv'], # Upload the diabetes csv files in /data
+                        target_path='diabetes-data/', # Put it in a folder path in the datastore
+                        overwrite=True, # Replace existing files of the same name
+                        show_progress=True)
+
+    #Create a tabular dataset from the path on the datastore (this may take a short while)
+    tab_data_set = Dataset.Tabular.from_delimited_files(path=(default_ds, 'diabetes-data/*.csv'))
+
+    # Register the tabular dataset
+    try:
+        tab_data_set = tab_data_set.register(workspace=ws, 
+                                name='diabetes dataset',
+                                description='diabetes data',
+                                tags = {'format':'CSV'},
+                                create_new_version=True)
+        print('Dataset registered.')
+    except Exception as ex:
+        print(ex)
+else:
+    print('Dataset already registered.')
+#
+
+#
+# delete cluster
+training.cluster.delete()
+#
---
+++ Azure_databricks_filestore/current tute.py
@@ -0,0 +1,3 @@
+# run in the command terminal
+# git clone https://github.com/MicrosoftLearning/mslearn-dp100
+
